1d489c530f3bd4dc7302cee34f5442a890130188
VERSACE-99 <tjdalsl1450@naver.com>
2024-10-08 10:36:43 +0900

    first commit

--- lib/ch8_3/test.dart
+++ lib/ch8_3/test.dart
@@ -5,40 +5,40 @@ void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  bool enabled = false;
-  String stateText = "disable";
+class MyApp extends StatelessWidget { // 정적 StatelessWidget을 상속받는 클래스 정의
+  bool enabled = false; // 체크박스의 활성화 상태를 관리하는 변수, 기본값 false.
+  String stateText = "disable"; // 상태에 따라 화면에 표시되는 텍스트 기본값 disable.
 
-  void changeChek() {
-    if (enabled) {
-      stateText = "disable";
-      enabled = false;
-    } else {
-      stateText = "enable";
-      enabled = true;
+  void changeChek() { // 체크박스 상태를 변경하는 함수.
+    if (enabled) { // enabled가 활성화 상태일 때.
+      stateText = "disable"; // 텍스트를 disable로 변경.
+      enabled = false; // 체크박스 비활성화.
+    } else { // enabled 비활성화.
+      stateText = "enable"; // 텍스트를 enable로 변경.
+      enabled = true; // 체크박스 활성화.
     }
   }
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
+      home: Scaffold( // 레이아웃 제공.
+        appBar: AppBar( // 제목 표시줄.
           title: Text('Stateless Test'),
         ),
-        body: Center(
-          child: Row(
+        body: Center( // 화면 중앙에 위젯 배치.
+          child: Row( // 수평 위젯 나열.
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              IconButton(
-                icon: (enabled ? Icon(Icons.check_box, size: 20,) :
-                      Icon(Icons.check_box_outline_blank, size: 20,)),
-                color: Colors.red,
-                onPressed: changeChek,
+              IconButton( // 클릭할 수 있는 아이콘 버튼 생성.
+                icon: (enabled ? Icon(Icons.check_box, size: 20,) : // true : 체크박스 아이콘 표시.
+                      Icon(Icons.check_box_outline_blank, size: 20,)), // false : 빈 체크박스 아이콘 표시.
+                color: Colors.red, // 색 설정.
+                onPressed: changeChek, // changecheck함수 호출 -> 상태 변경.
               ),
-              Container(
+              Container( // 텍스트 표시 컨테이너
                 padding: EdgeInsets.only(left: 16),
-                child: Text('$stateText', style: TextStyle(fontSize: 30,
+                child: Text('$stateText', style: TextStyle(fontSize: 30, // 택스트 표시.
                         fontWeight: FontWeight.bold),),
 
               ),
--- lib/ch8_4/test.dart
+++ lib/ch8_4/test.dart
@@ -4,13 +4,13 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { // statelessWidget(정적) UI상속.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('202116013_권성민 StatefulWidget test'),
+      home: Scaffold( // 레이아웃 설정.
+        appBar: AppBar( // 제목 표시줄.
+          title: Text('202116013_권성민 StatefulWidget test'), // 상단에 제목 표시.
         ),
         body: MyWidget(),
       ),
@@ -18,57 +18,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyWidget extends StatefulWidget {
+class MyWidget extends StatefulWidget { // StatefulWidget을 상속하여 상태를 가질 수 있는 위젯.
   @override
-  State<StatefulWidget> createState() {
+  State<StatefulWidget> createState() { // build함수가 없으므로 State를 상속받은 클래스 반환.
     return _MyWidgetState();
   }
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  bool enabled = false;
-  String stateText = "disable";
+  bool enabled = false; // 체크박스 상태 관리 변수, 기본값은 비활성.
+  String stateText = "disable"; // 화면에 표시될 텍스트.
 
-  void changeCheck() {
-    setState(() {
-      if (enabled) {
-        stateText = "disable";
-        enabled = false;
-      } else {
-        stateText = "enable";
-        enabled = true;
+  void changeCheck() { // 체크박스 상태 변경 함수.
+    setState(() { // 상태변경을 Flutter terminal에 보여줌.
+      if (enabled) { // 체크박스 활성화.
+        stateText = "disable"; // 텍스트 disable출력.
+        enabled = false; // 체크박스 비활성화.
+      } else { // 체크박스 비활성화.
+        stateText = "enable"; // 텍스트 enble 출력.
+        enabled = true; // 체크박스 활성화.
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: (enabled
-                  ? Icon(
-                Icons.check_box,
-                size: 20,
-              )
-                  : Icon(
-                Icons.check_box_outline_blank,
-                size: 20,
-              )),
-              color: Colors.red,
-              onPressed: changeCheck,
+    return Center( // 화면에 중앙 배치.
+      child: Row( // 위젯 수평 나열.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            icon: (enabled // enabled상태에 따라 다른 아이콘 표시.
+                ? Icon( // 체크박스 아이콘 표시.
+                    Icons.check_box,
+                    size: 20, // 아이콘 크기 설정.
+                  )
+                : Icon(
+                    Icons.check_box_outline_blank, // 빈 체크박스 아이콘 표시.
+                    size: 20,
+                  )),
+            color: Colors.red, // 색 설정.
+            onPressed: changeCheck, // 아이콘 클릭 시 changecheck 함수 호출.
+          ),
+          Container( // 텍스트를 포함할 컨테이너 함수.
+            padding: EdgeInsets.only(left: 16),
+            child: Text(
+              '$stateText', // 현재 상태에 따라 표시되는 텍스트
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), // 텍스트 스타일.
             ),
-            Container(
-              padding: EdgeInsets.only(left: 16),
-              child: Text(
-                '$stateText',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
