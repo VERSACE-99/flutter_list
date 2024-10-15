7bc4034c92dff25462026e7d6bbcef1a3552aef4
VERSACE-99 <tjdalsl1450@naver.com>
2024-10-15 11:50:51 +0900

    7w 10.15

---
+++ lib/http_ex/test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HttpApp(),
+    );
+  }
+}
+
+class HttpApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HttpApp();
+  }
+}
+
+class _HttpApp extends State<HttpApp> {
+  String result = ' ';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('202116013_권성민_Http Ex'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('$result'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var url = 'http://www.google.com';
+          var response = await http.get(Uri.parse(url));
+          setState(() {
+            result = response.body;
+          });
+        },
+        child: Icon(Icons.file_download),
+      ),
+    );
+  }
+}
