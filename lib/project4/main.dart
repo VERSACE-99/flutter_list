24f87f4e924a5b3279f087fea33f209b7bc21dcd
VERSAVE-99 <tjdalsl1450@naver.com>
2024-11-16 22:39:39 +0900

    '11.16'

---
+++ lib/project4/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_lab_202116013/project4/screen/home_screen.dart';
+import 'package:flutter_lab_202116013/project4/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
